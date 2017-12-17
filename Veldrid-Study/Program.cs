1baa807c281ed4775eb26dc3ea75903207fb8cda
phillyai <sonagi.philly@gmail.com>
2017-12-17 21:22:45 +0900

    초기 커밋.

---
+++ Veldrid-Study/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Veldrid_Study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
