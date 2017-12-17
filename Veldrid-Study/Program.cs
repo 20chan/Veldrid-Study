d2af78d80d5429a7ea7eadecf0229c20c75c5e90
phillyai <sonagi.philly@gmail.com>
2017-12-17 22:17:10 +0900

    Getting Start 튜토리얼 끝냄

--- Veldrid-Study/Program.cs
+++ Veldrid-Study/Program.cs
@@ -1,12 +1,127 @@
-﻿using System;
+﻿using System.IO;
+using System.Numerics;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+using Buffer = Veldrid.Buffer;
 
 namespace Veldrid_Study
 {
     class Program
     {
+        private static CommandList _commandList;
+        private static Buffer _vertexBuffer;
+        private static Buffer _indexBuffer;
+        private static Shader _vertexShader;
+        private static Shader _fragmentShader;
+        private static Pipeline _pipeline;
+        private static GraphicsDevice _graphicsDevice;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var info = new WindowCreateInfo()
+            {
+                X = 100,
+                Y = 100,
+                WindowWidth = 960,
+                WindowHeight = 540,
+                WindowTitle = "Tutorial"
+            };
+            var window = VeldridStartup.CreateWindow(ref info);
+            _graphicsDevice = VeldridStartup.CreateGraphicsDevice(window);
+
+            CreateResources();
+
+            while (window.Exists)
+            {
+                window.PumpEvents();
+                Draw();
+            }
+        }
+
+        static void Draw()
+        {
+            _commandList.Begin();
+            _commandList.SetFramebuffer(_graphicsDevice.SwapchainFramebuffer);
+            _commandList.SetFullViewports();
+            _commandList.ClearColorTarget(0, RgbaFloat.Black);
+            _commandList.ClearDepthStencil(1);
+            _commandList.SetVertexBuffer(0, _vertexBuffer);
+            _commandList.SetIndexBuffer(_indexBuffer, IndexFormat.UInt16);
+            _commandList.SetPipeline(_pipeline);
+            _commandList.DrawIndexed(
+                indexCount: 4,
+                instanceCount: 1,
+                indexStart: 0,
+                vertexOffset: 0,
+                instanceStart: 0);
+            _commandList.End();
+            _graphicsDevice.ExecuteCommands(_commandList);
+            _graphicsDevice.SwapBuffers();
+        }
+
+        static void CreateResources()
+        {
+            var factory = _graphicsDevice.ResourceFactory;
+
+            VertexPositionColor[] vertices =
+            {
+                new VertexPositionColor(new Vector2(-.75f, .75f), RgbaFloat.Red),
+                new VertexPositionColor(new Vector2(.75f, .75f), RgbaFloat.Green),
+                new VertexPositionColor(new Vector2(-.75f, -.75f), RgbaFloat.Blue),
+                new VertexPositionColor(new Vector2(.75f, -.75f), RgbaFloat.Yellow)
+            };
+
+            ushort[] indicies = { 0, 1, 2, 3 };
+            _vertexBuffer = factory.CreateBuffer(new BufferDescription(4 * VertexPositionColor.SizeInBytes, BufferUsage.VertexBuffer));
+            _indexBuffer = factory.CreateBuffer(new BufferDescription(4 * sizeof(uint), BufferUsage.IndexBuffer));
+            _graphicsDevice.UpdateBuffer(_vertexBuffer, 0, vertices);
+            _graphicsDevice.UpdateBuffer(_indexBuffer, 0, indicies);
+
+            var description = new VertexLayoutDescription(
+                new VertexElementDescription("Position", VertexElementSemantic.Position, VertexElementFormat.Float2),
+                new VertexElementDescription("Color", VertexElementSemantic.Color, VertexElementFormat.Float4));
+
+            _vertexShader = LoadShader(ShaderStages.Vertex);
+            _fragmentShader = LoadShader(ShaderStages.Fragment);
+
+            var pipelineDescription = new GraphicsPipelineDescription()
+            {
+                BlendState = BlendStateDescription.SingleOverrideBlend,
+                DepthStencilState = new DepthStencilStateDescription(true, true, ComparisonKind.LessEqual),
+                RasterizerState = new RasterizerStateDescription(FaceCullMode.Back, PolygonFillMode.Solid, FrontFace.Clockwise, true, false),
+                PrimitiveTopology = PrimitiveTopology.TriangleStrip,
+                ResourceLayouts = System.Array.Empty<ResourceLayout>(),
+                ShaderSet = new ShaderSetDescription(new[] { description }, new[] { _vertexShader, _fragmentShader }),
+                Outputs = _graphicsDevice.SwapchainFramebuffer.OutputDescription
+            };
+
+            _pipeline = factory.CreateGraphicsPipeline(pipelineDescription);
+            _commandList = factory.CreateCommandList();
+        }
+
+        static Shader LoadShader(ShaderStages stage)
+        {
+            string extension = null;
+            switch (_graphicsDevice.BackendType)
+            {
+                case GraphicsBackend.Direct3D11:
+                    extension = "hlsl.bytes";
+                    break;
+                case GraphicsBackend.Vulkan:
+                    extension = "spv";
+                    break;
+                case GraphicsBackend.OpenGL:
+                    extension = "glsl";
+                    break;
+                default: throw new System.InvalidOperationException();
+            }
+
+            string entryPoint = stage == ShaderStages.Vertex ? "VS" : "FS";
+            string path = Path.Combine(System.AppContext.BaseDirectory, "Shaders", $"{stage.ToString()}.{extension}");
+            byte[] shaderBytes = File.ReadAllBytes(path);
+            return _graphicsDevice.ResourceFactory.CreateShader(new ShaderDescription(stage, shaderBytes, entryPoint));
         }
     }
 }
---
+++ Veldrid-Study/VertexPositionColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace Veldrid_Study
+{
+    public struct VertexPositionColor
+    {
+        public Vector2 Position;
+        public RgbaFloat Color;
+
+        public VertexPositionColor(Vector2 pos, RgbaFloat color)
+        {
+            Position = pos;
+            Color = color;
+        }
+
+        public const uint SizeInBytes = 24;
+    }
+}
